7b4dabf6640a98915c04693f7dcbb8bd80908326
John Long <john@128.io>
2024-09-15 19:20:57 -0500

    Print caller identity

---
+++ .github/scripts/bash.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Authenticate to AWS using Github OIDC
+IDENTITY_TOKEN=$(
+  curl \
+    --verbose \
+    --header "Authorization: Bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" \
+    --header "User-Agent: actions/oidc-client" \
+    --url-query "audience=sts.amazonaws.com" \
+    $ACTIONS_ID_TOKEN_REQUEST_URL \
+  | jq -r .value
+)
+
+# Use the ID token to authenticate to AWS
+AWS_CREDS=$(
+  aws sts assume-role-with-web-identity \
+    --role-arn $ROLE_ARN \
+    --role-session-name GitHubActions \
+    --web-identity-token "$IDENTITY_TOKEN" \
+    --region $REGION
+)
+
+# Set the AWS credentials as environment variables
+export AWS_ACCESS_KEY_ID=$(echo $AWS_CREDS | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $AWS_CREDS | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $AWS_CREDS | jq -r .Credentials.SessionToken)
+
+# Fetch STS identity
+aws sts get-caller-identity
